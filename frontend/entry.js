5d4a1d72c8dfc3124340aaf965c766eef70567dd
carlaberg <mail.carl.aberg@gmail.com>
2020-01-18 23:03:52 +0100

    Trying with backend and frontend folder

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  Netlify frontend, check console for response from serverless backend
-  <script>
     // hämtar från api-endpoint "/"
     fetch('/.netlify/functions/api')
     .then(response => {
@@ -32,6 +21,3 @@
     .catch(err => {
       console.log(err)
     })
-  </script>
-</body>
-</html>
